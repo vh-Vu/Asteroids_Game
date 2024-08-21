07b8e12d95d666fa891d391ed92d664043a0ff7d
Hardiness7387 <huynh-vu@outlook.com>
2024-08-22 00:38:29 +0700

    add circle shape and create infinity loop

--- main.py
+++ main.py
@@ -2,24 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import sys
 from constants import *
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 def main():
-    
-    print('Starting asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        screen.fill((244,244,244))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
+        screen.fill("black")
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
