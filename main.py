298ec1381b3264fb60de676fac7fccc05aaa08e9
Hardiness7387 <huynh-vu@outlook.com>
2024-08-21 15:02:03 +0700

    setup enviroment and and pygame

---
+++ main.py
@@ -0,0 +1,13 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+def main():
+    print('Starting asteroids!')
+
+
+
+
+
+if __name__ == "__main__":
+    main()
