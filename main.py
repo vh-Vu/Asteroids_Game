c774c3f7d7d6cb9522e492ef0359646cd724ab9c
Hardiness7387 <huynh-vu@outlook.com>
2024-08-21 15:30:04 +0700

    testing

--- main.py
+++ main.py
@@ -2,10 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
+    
     print('Starting asteroids!')
-
-
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        screen.fill((255,255,255))
+        pygame.display.flip()
 
 
 
